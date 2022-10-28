671b60fc1dce45e6e40cf6e7d878ffe086e56a2c
syam187 <dennohsyam12@gmail.com>
2022-10-28 10:41:21 +0300

    updates

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,23 @@
 <?php
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\HomeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// check on busta portal how to write controllers
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class,'index']);
+
+Route::get('/home', [HomeController::class,'index']);
+
+Route::prefix('admin')->group(function() {
+
+    Route::get('/dashboard', [DashboardController::class,'index']);
+    
+});
